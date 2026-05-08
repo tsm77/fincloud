f699e649cde7591341d49f2586253918ea112287
Tiago S Martins <tiagosantiagotsm77@gmail.com>
2026-05-08 20:46:25 -0300

    alterando apontamento para o backend

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,9 +1,9 @@
-export const environmentProd = {
+export const environment = {
   production: true,
   apiUrl: 'https://fincloud-backend.onrender.com',
 };
 
-export const environment = {
+export const environmentDev = {
   production: false,
   apiUrl: 'http://localhost:8080',
 };
